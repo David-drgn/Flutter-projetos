f489f6545592fa87c6e8474f9f7525d484c7b2de
David_drgn <72632974+David-drgn@users.noreply.github.com>
2022-09-29 23:50:58 -0300

    Delete navegacao directory

--- navegacao/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:navegacao/view/home.dart';
-import 'package:navegacao/view/secundario.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
